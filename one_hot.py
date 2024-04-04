3ca1cda644385ffca002a9e5c38c64e1c5f9dc30
PatchiMari <151833926+PatchiMari@users.noreply.github.com>
2024-04-04 20:50:55 +0300

    Update one_hot.py

--- one_hot.py
+++ one_hot.py
@@ -1,12 +1,13 @@
-import pandas as pd
+import pandas as pd 
 import random
 
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
-
 data = pd.DataFrame({'whoAmI': lst})
 
-one_hot = pd.get_dummies(data, columns=['whoAmI'])
+dt_one_hot = data.apply(lambda y: y.astype('category').cat.codes)
+dt_one_hot = pd.concat([dt_one_hot, 1 - dt_one_hot], axis=1)
+dt_one_hot.columns = ['human', 'robot']
 
-print(one_hot.head())
+dt_one_hot.head()
